1da7233dbb7133bef32d6aefe0309cad2af72e77
heswaran3 <heswaran3@192.168.27.106>
2018-07-30 22:48:12 -0700

    Initial Commit

---
+++ src/main/java/com/example/sfwpetclinic/SfwPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.example.sfwpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfwPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfwPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/sfwpetclinic/SfwPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sfwpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfwPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
